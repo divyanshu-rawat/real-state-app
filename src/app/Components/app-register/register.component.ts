7bb73b1ad622a6fb79fd7ca9ae4f7b7ad56ee38b
Divyanshu <divyanshu.r46956@gmail.com>
2019-07-20 20:48:57 +0200

    Added component for user reg.

---
+++ src/app/Components/app-register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../Services/AuthenticationService/authentication.service';
+import { userForm } from '../../Models/userForm';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.sass']
+})
+export class RegisterComponent implements OnInit {
+  private credentials: userForm = {
+    email: '',
+    username: '',
+    password: ''
+  };
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+  ngOnInit() { };
+  register() {
+    this.authenticationService.registerUser(this.credentials).subscribe((response) => {
+      if (response.success) {
+        this.router.navigate(['/dashboard']);
+      }
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+}
