6cf28b6c959991cef341cce2dec7fc7240c22f27
Divyanshu <divyanshu.r46956@gmail.com>
2019-07-20 20:49:51 +0200

    Added bootstrap 3, login component, home component.

---
+++ src/app/Components/app-login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { userForm } from '../../Models/userForm';
+import { AuthenticationService } from '../../Services/AuthenticationService/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  credentials: userForm = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginUser(this.credentials).subscribe((response) => {
+      console.log("response", response);
+      this.router.navigate(['/dashboard']);
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+}
